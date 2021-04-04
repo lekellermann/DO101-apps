640f971391afaca262de8607a7a829e0954822e1
lekellermann <lekellermann@my.waketech.edu>
2021-04-04 15:58:43 +0000

    added comments to document backend methods

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -2,14 +2,31 @@ const express = require('express');
 const router = express.Router();
 const fetch = require("node-fetch");
 require('dotenv').config();
+
+/* API key for the OpenWeatherMap API, injects the key
+   as an environment variable at run time.
+*/
 const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key';
+
+/* Calls the configurration map, controls if the weather forecast
+   is displayed in metric(degree Celsius) or imperial units(fahrenheit)
+*/
 const UNITS = process.env.UNITS || 'metric';
 
-/* GET home page. */
+/* GET home page. 
+   All HTTP GET Requests to the '/' URL are 
+   redirected to a page with an HTML form that allows you to enter
+   a city name for which you want the weather forecast */
 router.get('/', function(req, res) {
   res.render('index', { weather: null, err: null });
 });
 
+
+/*
+    Handles HTTP POST requests from the HTML form 
+    by invoking the OpenWeatherMap API, then passes the resulting
+    JSON response to the HTML front end:
+*/
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
